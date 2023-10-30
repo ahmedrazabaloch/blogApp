3eb29f33f7fe72a105c9137450740c4a2226d164
ahmedraza <ahmed.raza1054@gmail.com>
2023-10-31 03:15:10 +0500

    code push

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ var editToolBar = [
   ["bold", "italic", "underline", "strike"],
   [{ header: 1 }, { header: 2 }],
   [{ color: ["#05386b", false] }, { background: [] }],
+  [{ size: ["small", false, "large", "huge"] }],
+  [{ direction: "rtl" }],
   ["blockquote", "code-block"],
   [{ align: [] }],
   ["image"],
+  ["clean"],
 ];
 
 var quill = new Quill("#editor", {
   modules: {
     toolbar: editToolBar,
   },
+  placeholder: "Write as You've Never Written Before",
   theme: "snow",
 });
 
-console.log(quill.getText());
+var customButton = document.querySelector("#custom-button");
+customButton.addEventListener("click", function () {
+  console.log("Clicked!");
+});
+
+// console.log(quill.getText());
