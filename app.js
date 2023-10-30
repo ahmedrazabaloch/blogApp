fa12889878288db4f22ff150701c446b06326908
ahmedraza <ahmed.raza1054@gmail.com>
2023-10-30 21:09:58 +0500

    code push

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ var quill = new Quill("#editor", {
   theme: "snow",
 });
 
-$("#saveDelta").click(function () {
-  window.delta = quill.contents();
-  console.log(window.delta);
+// var editor_content = quill.container.innerHTML; // or quill.container.firstChild.innerHTML could also work
+// var textValue = quill.container.firstChild.innerHTML;
+
+// setInterval(() => {
+//   console.log(textValue.value);
+// },2000);
+
+var saveDelta = document.getElementById("saveDelta");
+saveDelta.addEventListener("click", function () {
+  var quillValue = quill.getText();
+  console.log(quillValue);
 });
